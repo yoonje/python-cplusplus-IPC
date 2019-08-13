c19111d5986201838163214e857e5fbf21dc874c
yoonje <elxm6123@gmail.com>
2019-08-14 02:39:59 +0900

    real final commit

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 ### IPC 
-Between Python(Client) process and C++(Server) process socket communication.
+Between Python(Client) process and C++(Server) process socket communication.<br>
+
+IPC was communicated by socket file(.sock).
 
 ### Usage
 You should run C++ process before run Python process.
 
+C++ server run infinitely, Python Client run once.
+
 ### My Setting
 HOST = localhost<br>
 PORT = 5000<br>
-SOCKET_PATH = "/home/yoonje/PycharmProjects/python_socket/C++server/server.dat"
+SOCKET_PATH = "/home/yoonje/PycharmProjects/python_socket/C++server/server.dat"<br>
+
+You can exchange setting according to your preference.
 
 ### Socket Progress
 - TCP/IP communication function order<br>
--- python-client/client.py
+++ python-client/client.py
@@ -2,23 +2,21 @@ import socket
 
 HOST = 'localhost'
 PORT = 5000
-SOCKET_PATH = "/home/yoonje/PycharmProjects/python_socket/C++server/server.dat"  # Socket file location
+SOCKET_PATH = "/home/yoonje/PycharmProjects/python_socket/C++server/connection.sock"  # Socket file location
 BUFF_SIZE = 1024
 
 try:
-    client_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)  # AF_UNIX means local communication, SOCK_STREAM means TCP
+    client_sock = socket.socket(socket.AF_UNIX,
+                                socket.SOCK_STREAM)  # AF_UNIX means local communication, SOCK_STREAM means TCP
     client_sock.connect(SOCKET_PATH)
     print("Socket Connected.")
 except socket.error as err:
     print("error due to: %s" % err)
     exit(0)
 
-while True:
-    data = input(">>")
-    client_sock.sendall(data.encode())
-    print("SEND:", data.encode())
-    if data == "END":
-        client_sock.close()
-        break
-    res = client_sock.recv(BUFF_SIZE)
-    print("RECEIVE:", res.decode())
+data = input(">>")
+client_sock.sendall(data.encode())
+print("SEND:", data)
+res = client_sock.recv(BUFF_SIZE)
+print("RECEIVE:", res.decode())
+client_sock.close()
